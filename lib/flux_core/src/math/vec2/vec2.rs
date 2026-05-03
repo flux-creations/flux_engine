180c774ea0a7e54cdf122123216b28b333b9b7a3
0zminDev <0zminDev@pm.me>
2026-05-03 11:44:05 +0200

    add vec2 ops

--- lib/flux_core/src/math/vec2/vec2.rs
+++ lib/flux_core/src/math/vec2/vec2.rs
@@ -8,6 +8,8 @@ trait VectorSpace<T: Num, S: Num> {
 
     fn parse<S>(self: Self<T>) -> Option<Self<S>>;
 
+    fn magnitude<T>(self: Self<T>) -> T;
+
     fn invert<T>(self: Self<T>) -> Self<T>;
 
     fn add<T>(self: Self<T>, other: Self<T>) -> Self<T>;
@@ -18,6 +20,22 @@ trait VectorSpace<T: Num, S: Num> {
 struct Vec2<T: Num> {
     x: T,
     y: T,
+}
+
+struct Vec3<T: Num> {
+    x: T,
+    y: T,
+    z: T
+}
+
+impl VectorSpace for Vec3<T: Num, S: Num> {
+    fn new<T>(x: T, y: T, z: T) -> Vec3 {
+        Vec3 {
+            x: x,
+            y: y,
+            z: z
+        }
+    }
 }
 
 impl VectorSpace for Vec2<T: Num, S: Num> {
@@ -52,7 +70,15 @@ impl VectorSpace for Vec2<T: Num, S: Num> {
         Vec2::new<T>(-self.x, -self.y)
     }
 
-    fn add<T>(self: Self<T>, other: Self<T>) -> Self<T>;
-    fn dot<T>(self: Self<T>, other: Self<T>) -> Self<T>;
-    fn cross<T>(self: Self<T>, other: Self<T>) -> Self<T>;
+    fn add<T>(self: Vec2<T>, other: Vec2<T>) -> Vec2<T> {
+        Vec2::new<T>(self.x + other.x, self.y + other.y);
+    }
+
+    fn dot<T>(self: Vec2<T>, other: Vec2<T>) -> Vec2<T> {
+        self.x * other.x + self.y * other.y
+    }
+
+    fn cross<T>(self: Vec2<T>, other: Vec2<T>) -> Vec3<T> {
+        Vec3<T>::new(0, 0, self.x * other.y - (self.y * other.x))
+    }
 }
