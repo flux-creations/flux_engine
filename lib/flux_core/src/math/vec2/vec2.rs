05f6ef0948f87e08cb588e1c4ddd820d9f70e9be
0zminDev <0zminDev@pm.me>
2026-05-04 11:01:46 +0200

    Added magnitude and cross product

--- lib/flux_core/src/math/vec2/vec2.rs
+++ lib/flux_core/src/math/vec2/vec2.rs
@@ -1,9 +1,11 @@
-use num::Num;
+use num::{Num, ToPrimitive};
 
-pub trait VectorSpace<T: Num>
+pub trait VectorSpace<T: Num + ToPrimitive + Copy>
 where
     Self: Sized,
 {
+    type CrossOutput; // Associated type i implement this for each struct impl
+
     fn zero() -> Self;
     fn one() -> Self;
     fn up() -> Self;
@@ -17,35 +19,37 @@ where
 
     fn add(self, other: Self) -> Self;
     fn dot(self, other: Self) -> T;
-    fn cross(self, other: Self) -> Self;
+    fn cross(self, other: Self) -> Self::CrossOutput;
 }
 
 #[derive(Debug, Copy, Clone)]
-pub struct Vec2<T: Num> {
+pub struct Vec2<T: Num + ToPrimitive + Copy> {
     x: T,
     y: T,
 }
 
 #[derive(Debug, Copy, Clone)]
-pub struct Vec3<T: Num> {
+pub struct Vec3<T: Num + ToPrimitive + Copy> {
     x: T,
     y: T,
     z: T,
 }
 
-impl<T: Num> Vec3<T> {
+impl<T: Num + ToPrimitive + Copy> Vec3<T> {
     pub fn new(x: T, y: T, z: T) -> Vec3<T> {
         Vec3 { x, y, z }
     }
 }
 
-impl<T: Num> Vec2<T> {
+impl<T: Num + ToPrimitive + Copy> Vec2<T> {
     pub fn new(x: T, y: T) -> Vec2<T> {
         Vec2 { x, y }
     }
 }
 
-impl<T: Num> VectorSpace<T> for Vec3<T> {
+impl<T: Num + ToPrimitive + Copy> VectorSpace<T> for Vec3<T> {
+    type CrossOutput = Vec3<T>;
+
     fn zero() -> Self {
         unimplemented!()
     }
@@ -91,9 +95,15 @@ impl<T: Num> VectorSpace<T> for Vec3<T> {
     }
 }
 
-impl<T: Num> VectorSpace<T> for Vec2<T> {
+impl<T: Num + ToPrimitive + Copy> VectorSpace<T> for Vec2<T> {
+    type CrossOutput = Vec3<T>;
+
     fn magnitude(self) -> f64 {
-        f64::sqrt((self.x * self.x + self.y * self.y) as f64)
+        f64::sqrt(
+            (self.x * self.x + self.y * self.y)
+                .to_f64()
+                .expect("The conversion to f64 was not possible!"),
+        )
     }
 
     fn zero() -> Vec2<T> {
@@ -129,11 +139,9 @@ impl<T: Num> VectorSpace<T> for Vec2<T> {
     fn dot(self: Vec2<T>, other: Vec2<T>) -> T {
         self.x * other.x + self.y * other.y
     }
-    // TODO: Why this shows Vec2??
-    fn cross(self: Vec2<T>, other: Vec2<T>) -> Vec2<T> {
-        _ = other;
-        //Vec3::new(T::zero(), T::zero(), (self.x * other.y - (self.y * other.x) as T))
-        unimplemented!()
+
+    fn cross(self: Vec2<T>, other: Vec2<T>) -> Vec3<T> {
+        Vec3::new(T::zero(), T::zero(), self.x * other.y - (self.y * other.x))
     }
 }
 
@@ -158,9 +166,48 @@ mod tests {
     fn test_vec2_addition() {
         let vec2: Vec2<i32> = Vec2::new(3, 5);
         let vec2_other: Vec2<i32> = Vec2::new(3, 0);
+
         let vec2_addition_result: Vec2<i32> = vec2.add(vec2_other);
+
         assert_eq!(type_of(vec2_addition_result), type_of(vec2));
         assert_eq!(vec2_addition_result.x, 6);
         assert_eq!(vec2_addition_result.y, 5);
+    }
+
+    #[test]
+    fn test_vec2_magnitude() {
+        let vec2: Vec2<i32> = Vec2::new(2, 2);
+
+        let vec2_magnitude = vec2.magnitude();
+
+        assert_eq!(type_of(vec2_magnitude), type_of(3.0_f64));
+        assert_eq!(vec2_magnitude, f64::sqrt(8_f64));
+    }
+
+    #[test]
+    fn test_vec2_dot() {
+        let vec2: Vec2<i32> = Vec2::new(2, 2);
+        let vec2_other: Vec2<i32> = Vec2::new(2, 2);
+
+        let vec2_dot_product_result: i32 = vec2.dot(vec2_other);
+
+        assert_eq!(type_of(vec2_dot_product_result), type_of(3_i32));
+        assert_eq!(vec2_dot_product_result, 8);
+    }
+
+    #[test]
+    fn test_vec2_cross() {
+        let vec2: Vec2<i32> = Vec2::new(2, 2);
+        let vec2_other: Vec2<i32> = Vec2::new(2, 2);
+
+        let vec2_cross_product_result: Vec3<i32> = vec2.cross(vec2_other);
+
+        assert_eq!(
+            type_of(vec2_cross_product_result),
+            type_of(Vec3::new(3_i32, 4_i32, 3_i32))
+        );
+        assert_eq!(vec2_cross_product_result.x, 0_i32);
+        assert_eq!(vec2_cross_product_result.y, 0_i32);
+        assert_eq!(vec2_cross_product_result.z, 0_i32);
     }
 }
